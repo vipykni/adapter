2d5426c6936da8723173e56153b3d89d986c7f0a
Maria K <vipykni@mail.ru>
2020-12-06 03:25:50 +0700

    Адаптирован

--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include <QCoreApplication>
 #include "duck.h"
 #include "mallardduck.h"
+#include "turkey.h"
+#include "turkeyadapter.h"
+#include "wildturkey.h"
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
     MallardDuck *duck = new MallardDuck();
+    WildTurkey *turkey = new WildTurkey();
+    Duck *turkeyAdapter = new TurkeyAdapter(turkey);
+
+    cout << "turkey:" << endl;
+    turkey ->gobble();
+    turkey ->fly();
+
+    cout << endl << "duck:" << endl;
+    duck ->quack();
+    duck ->fly();
+
+    cout << endl << "turkey adapter:" << endl;
+    turkeyAdapter ->quack();
+    turkeyAdapter ->fly();
+
+
+
+
+
 
     return a.exec();
 }
